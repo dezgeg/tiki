ec62f1bf32daf4006fd27a0a26568991574a8404
Tuomas Tynkkynen <dezgeg@gmail.com>
2013-05-05 19:45:33 +0300

    Fixmes.

--- include/valgrindExample.c
+++ include/valgrindExample.c
@@ -2,7 +2,9 @@ int main(void) {
     int undefined;
     int lowest_bit = undefined & 1;
     int x = undefined + lowest_bit * 2;
-    if (lowest_bit & 1)
-      printf("Tulostetaanko tämä?\n");
+    if (lowest_bit == 1)
+      printf("Alin bitti on asetettu\n");
+    else
+      printf("Alin bitti ei ole asetettu\n");
     return x;
 }
